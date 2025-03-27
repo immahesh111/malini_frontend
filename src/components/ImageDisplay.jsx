8397bcdb7845570ec08a53af46869f2691809856
immahesh555 <themahiece@gmail.com>
2025-03-27 11:55:42 +0530

    a

--- src/components/ImageDisplay.jsx
+++ src/components/ImageDisplay.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ImageDisplay = ({ imageUrl }) => {
   return (
     <div style={{ position: "absolute", top: "20%", right: "29%", zIndex: 10 }}>
-      <img src={imageUrl} alt="Guide step" style={{ width: "1050px", height: "auto" }} />
+      <img src={imageUrl} alt="Guide step" style={{ width: "800px", height: "auto" }} />
     </div>
   );
 };
