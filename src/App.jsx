389e60ef0beb161e265627d0452b071957f940db
immahesh555 <themahiece@gmail.com>
2025-03-28 15:14:25 +0530

    sswe

--- src/App.jsx
+++ src/App.jsx
@@ -131,7 +131,7 @@ function App() {
     };
 
     const recognizeFace = async () => {
-      const labels = ["aarushi", "Mahi","President Mr. Kamlesh Mishra","Jagdeesh Sir", "nandini","Pavan Sir","Bupinder Sir","tripti"];
+      const labels = ["aarushi", "Mahi","President Mr. Kamlesh Mishra","Jagdeesh Sir", "nandini","Pavan Sir","Bupinder Sir","tripti","Jagdish Sir","Jagdeesh Raj Sir"];
       const labeledDescriptors = await Promise.all(
         labels.map(async (label) => {
           try {
