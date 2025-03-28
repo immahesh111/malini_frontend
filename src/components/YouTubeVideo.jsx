4f7c511638efa30e827cd5d12e1582f626c9577a
immahesh555 <themahiece@gmail.com>
2025-03-28 09:47:57 +0530

    ss

--- src/components/YouTubeVideo.jsx
+++ src/components/YouTubeVideo.jsx
@@ -35,7 +35,7 @@ const YouTubeVideo = ({ videoId, onEnd }) => {
       style={{
         position: "absolute",
         top: "50%",
-        right: "50%",
+        left: "60%",
         transform: "translate(-50%, -50%)",
         width: "80vw",
         height: "80vh",
