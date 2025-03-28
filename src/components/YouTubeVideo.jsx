c4e7887291b7475134c9fea872fb0836b72ab9c3
immahesh555 <themahiece@gmail.com>
2025-03-28 13:24:50 +0530

    sds

--- src/components/YouTubeVideo.jsx
+++ src/components/YouTubeVideo.jsx
@@ -49,9 +49,74 @@
 
 // export default YouTubeVideo;
 
+// import { useEffect, useRef } from 'react';
+
+// function YouTubeVideo({ videoId, onEnd }) {
+//   const playerRef = useRef(null);
+//   const containerRef = useRef(null);
+
+//   useEffect(() => {
+//     const loadPlayer = () => {
+//       if (window.YT && window.YT.Player && containerRef.current) {
+//         playerRef.current = new window.YT.Player('youtube-player', {
+//           videoId: videoId,
+//           width: containerRef.current.clientWidth,
+//           height: containerRef.current.clientHeight,
+//           playerVars: { autoplay: 1, controls: 1 },
+//           events: {
+//             onStateChange: (event) => {
+//               console.log('Player state:', event.data);
+//               if (event.data === window.YT.PlayerState.ENDED) {
+//                 onEnd();
+//               }
+//             },
+//           },
+//         });
+//       }
+//     };
+
+//     if (window.YT && window.YT.Player) {
+//       loadPlayer();
+//     } else {
+//       window.onYouTubeIframeAPIReady = loadPlayer;
+//       if (!document.querySelector('script[src="https://www.youtube.com/iframe_api"]')) {
+//         const tag = document.createElement('script');
+//         tag.src = 'https://www.youtube.com/iframe_api';
+//         const firstScriptTag = document.getElementsByTagName('script')[0];
+//         firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+//       }
+//     }
+
+//     return () => {
+//       if (playerRef.current) {
+//         playerRef.current.destroy();
+//       }
+//     };
+//   }, [videoId, onEnd]);
+
+//   return (
+//     <div
+//       ref={containerRef}
+//       style={{
+//         position: 'absolute',
+//         top: '50%',
+//         left: '50%',
+//         transform: 'translate(-50%, -50%)',
+//         width: '60vw',
+//         height: '80vh',
+//         zIndex: 20,
+//       }}
+//     >
+//       <div id="youtube-player"></div>
+//     </div>
+//   );
+// }
+
+// export default YouTubeVideo;
+
 import { useEffect, useRef } from 'react';
 
-function YouTubeVideo({ videoId, onEnd }) {
+function YouTubeVideo({ videoId, onPlay, onEnd }) {
   const playerRef = useRef(null);
   const containerRef = useRef(null);
 
@@ -66,9 +131,14 @@ function YouTubeVideo({ videoId, onEnd }) {
           events: {
             onStateChange: (event) => {
               console.log('Player state:', event.data);
-              if (event.data === window.YT.PlayerState.ENDED) {
-                onEnd();
+              if (event.data === window.YT.PlayerState.PLAYING) {
+                onPlay(); // Call onPlay when video starts playing
+              } else if (event.data === window.YT.PlayerState.ENDED) {
+                onEnd(); // Call onEnd when video ends
               }
+            },
+            onError: (event) => {
+              console.error('YouTube player error:', event.data);
             },
           },
         });
@@ -92,7 +162,7 @@ function YouTubeVideo({ videoId, onEnd }) {
         playerRef.current.destroy();
       }
     };
-  }, [videoId, onEnd]);
+  }, [videoId, onPlay, onEnd]);
 
   return (
     <div
